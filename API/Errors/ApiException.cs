7e3baa8cf4dcfc9090743705a08a3da9b20e6229
kyawhtay <kyaw.htay@gmail.com>
2023-12-18 13:09:31 +0000

    Added middleware error exception

---
+++ API/Errors/ApiException.cs
@@ -0,0 +1,18 @@
+﻿namespace API;
+
+public class ApiException
+{
+        private readonly string message;
+
+    public ApiException(int statusCode,string message,string details)
+    {
+        Message = message;
+        Details = details;
+        StatusCode = statusCode;
+    }
+    public int StatusCode { get; set; }
+     public string Message { get; set; }
+
+     public string Details { get; set; }
+   
+}
