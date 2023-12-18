1e94ad883624863b43018629216a0288c76b757d
kyawhtay <kyaw.htay@gmail.com>
2023-12-18 00:07:51 +0000

    Added the routeguard

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_service/account.service';
 import { Observable, of } from 'rxjs';
 import { User } from '../_models/user';
+import {Router} from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -13,7 +15,7 @@ export class NavComponent implements OnInit{
 model:any ={};
 
 
-  constructor(public accountService:AccountService) {}
+  constructor(public accountService:AccountService,private router: Router,private toastr: ToastrService) {}
   ngOnInit(): void {
   
   }
@@ -21,15 +23,17 @@ model:any ={};
 
   login(){
     this.accountService.login(this.model).subscribe({
-      next: repsonse=> {
-        console.log(repsonse);
-        
-      },
-      error: error=> console.log(error)
+      next: ()=> 
+        this.router.navigateByUrl('/members'),
+      error: error=> {
+        console.log(error);
+        this.toastr.error(error.error)
+      }
     })
   }
   logout(){ 
     this,this.accountService.logout();
+    this.router.navigateByUrl('/')
    
   }
 
