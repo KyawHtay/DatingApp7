e4b7adc797f299210c14375370b5beab48e77ebc
KyawHtay <kyaw.htay@gmail.com>
2023-12-23 22:57:32 +0000

    Updated local post

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -54,7 +54,7 @@ namespace API.Controllers
         public async Task<ActionResult<UserDto>> login(LoginDto loginDto){
             var user = await _context.Users
                     .Include(x=>x.Photos)
-                    .SingleOrDefaultAsync(x=>x.UserName==loginDto.Username);
+                    .SingleOrDefaultAsync(x=>x.UserName==loginDto.Username.ToLower());
             if(user ==null) return Unauthorized();
             using var hmac= new HMACSHA512(user.PasswordSalt);
             var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
